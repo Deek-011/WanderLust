bb7ca5d6407279d324c00f9159cd5111a8f18909
Deek-011 <deekshithk276@gmail.com>
2025-07-09 15:34:40 +0530

    Update PhotosUploader.jsx

--- frontend/src/PhotosUploader.jsx
+++ frontend/src/PhotosUploader.jsx
@@ -47,7 +47,7 @@ function selectAsMainPhoto(ev,filename) {
       <div className=" mt-2 grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
         {addedPhotos.length > 0 && addedPhotos.map(link => (
           <div className="h-32 flex relative" key={link}>
-            <img className="rounded-2xl w-full object-cover" src={'http://localhost:5000/uploads/' + link} alt="" />
+            <img className="rounded-2xl w-full object-cover" src={'https://wanderlust-h5lq.onrender.com/uploads/' + link} alt="" />
             <button onClick={ev => removePhoto(ev,link)} className=" cursor-pointer absolute bottom-1 right-1 text-white bg-black bg-opacity-50 rounded-2xl py-2 px-3">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
                 <path fillRule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z" clipRule="evenodd" />
