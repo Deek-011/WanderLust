c0bc5612a37dd2ab946c32fa959ef9576f09b6ae
Deek-011 <deekshithk276@gmail.com>
2025-07-11 18:58:11 +0530

    Update BookingsPage.jsx

--- frontend/src/pages/BookingsPage.jsx
+++ frontend/src/pages/BookingsPage.jsx
@@ -10,7 +10,7 @@ import BookingDates from "../BookingDates";
 export default function BookingsPage() {
   const [bookings, setBookings] = useState([]);
   useEffect(() => {
-    axios.get('/api/bookings').then(response => {
+    axios.get('/api/bookings',{ withCredentials: true }).then(response => {
       setBookings(response.data);
     });
   }, []);
